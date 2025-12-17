101b95a1eb38fc8a9bd94f6613730abf9c25e4e4
GlukhovMaksim <gluxov-1990@mail.ru>
2025-12-17 21:43:23 +0500

    fix review

--- packages/client/src/pages/Game/Game.tsx
+++ packages/client/src/pages/Game/Game.tsx
@@ -6,7 +6,15 @@ import styles from './Game.module.css'
 const Game = () => {
   const [endGame, setEndGame] = useState(false)
 
-  return <div className={styles.wrapper}>{endGame && <GameOverScreen />}</div>
+  const handleRepeatGame = () => {
+    console.log('Repeat game')
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      {endGame && <GameOverScreen repeatGame={handleRepeatGame} />}
+    </div>
+  )
 }
 
 export default Game
--- packages/client/src/pages/GameOverScreen/GameOverScreen.tsx
+++ packages/client/src/pages/GameOverScreen/GameOverScreen.tsx
@@ -5,11 +5,17 @@ import { Button } from '@/components'
 import classes from './GameOverScreen.module.css'
 import { ROUTES } from '@/constants/routes'
 
-export const GameOverScreen = () => {
+type GameOverScreenProps = {
+  repeatGame: () => void
+}
+
+export const GameOverScreen = ({ repeatGame }: GameOverScreenProps) => {
   return (
     <section className={classes.container}>
       <h1 className={classes.title}>Конец игры</h1>
-      <Button className={classes.repeatBtn}>Повторить</Button>
+      <Button onClick={repeatGame} className={classes.repeatBtn}>
+        Повторить
+      </Button>
       <Link to={ROUTES.PUBLIC.HOME} className={classes.backBtn}>
         Вернуться в главное меню
       </Link>
