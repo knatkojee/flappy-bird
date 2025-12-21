3565a43daf265d40d813de57ca9b9681b2b99a20
Baktiyar Ishenov <pixelbaha@gmail.com>
2025-12-21 23:31:51 +0600

    11 Реализовать логику авторизации (#39)
    * Реализовать логику авторизации

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,6 +3,10 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './routes/routes'
 import './App.css'
 import { ApplicationLayout, LoadingSpinner } from '@/components'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useAppDispatch } from './hooks/useAppDispatch'
+import { fetchUser } from './store/authSlice'
 
 const PageLayout = () => {
   return (
@@ -15,20 +19,16 @@ const PageLayout = () => {
 }
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
+  const dispatch = useAppDispatch()
 
-    fetchServerData()
-  }, [])
+  useEffect(() => {
+    dispatch(fetchUser())
+  }, [dispatch])
 
   return (
     <Router>
       <PageLayout />
+      <ToastContainer />
     </Router>
   )
 }
