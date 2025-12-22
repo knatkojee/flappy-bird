c0232374bcc4aed0007a8e2bf54e4ca836004fe6
knatkojee <knatkojee@yandex.ru>
2025-12-22 21:34:48 +0300

    tests fix

--- packages/client/src/setupTests.ts
+++ packages/client/src/setupTests.ts
@@ -1,5 +1,7 @@
 import '@testing-library/jest-dom'
+import { TextEncoder, TextDecoder } from 'util'
 
-// Polyfill for TextEncoder/TextDecoder
-global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder as any
+  global.TextDecoder = TextDecoder as any
+}
