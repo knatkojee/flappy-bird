b1f70c1109aeff8ce68cb5fe38bddc4ac81859b3
knatkojee <knatkojee@yandex.ru>
2026-02-07 12:19:53 +0300

    Удалил ненужные файлы

--- shared/server/renderer.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Request } from 'express'
-import type { Store } from 'redux'
-import { store } from '@store/store'
-export declare const initializeServerStore: (req: Request) => Promise<{
-  store: Store
-  initialState: any
-}>
-export declare const serializeStateForClient: (state: any) => string
-//# sourceMappingURL=renderer.d.ts.map
