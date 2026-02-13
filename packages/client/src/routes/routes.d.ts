bb84e9b9904cc88dc325354f67e614a813592b8b
CroKodila <kakunin.k.i@gmail.com>
2026-02-13 16:09:28 +0300

    more build files are removed

--- packages/client/src/routes/routes.d.ts
+++
@@ -1,4 +0,0 @@
-import { ROUTES } from '../constants/routes'
-export declare type RouteKeys = keyof typeof ROUTES
-declare const AppRoutes: () => import('react/jsx-runtime').JSX.Element
-export default AppRoutes
--- packages/client/src/routes/types.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ComponentType } from 'react'
-export declare type PageWithTitleProps<T extends object = object> = {
-  component: ComponentType<T>
-  title: string
-  componentProps?: T
-}
