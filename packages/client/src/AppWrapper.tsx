1978cbdf377bd106bd1355826cbd045ae725f743
knatkojee <knatkojee@yandex.ru>
2026-02-16 19:08:26 +0300

    Фикс вечной загрузки

--- packages/client/src/AppWrapper.tsx
+++ packages/client/src/AppWrapper.tsx
@@ -1,13 +1,21 @@
 import type { ReactNode } from 'react'
-import { Suspense } from 'react'
+import { Suspense, useEffect } from 'react'
 import AppRoutes from './routes/routes'
 import './App.css'
 import { ApplicationLayout, LoadingSpinner } from '@/components'
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { useAppDispatch } from './hooks/useAppDispatch'
+import { fetchUser } from '@shared'
 
 const PageLayout = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchUser())
+  }, [dispatch])
+
   return (
     <ApplicationLayout>
       <Suspense fallback={<LoadingSpinner />}>
